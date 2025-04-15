d35cd9bfcb4c145a9eac35d0f097361314fc3f07
Sumit Pawar <sp260089@gmail.com>
2025-04-15 08:21:02 +0530

    Added Room Dependency & Created Room Entity file(Note.kt)

---
+++ app/src/main/java/com/example/room_noteapp/Note.kt
@@ -0,0 +1,15 @@
+package com.example.room_noteapp
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "note_table")
+data class Note(
+    var title:String,               //Column names
+    var description:String,
+    var age:Int
+){
+    //Primary key(autoIncrement)
+    @PrimaryKey(autoGenerate = true)
+    var id=0
+}
