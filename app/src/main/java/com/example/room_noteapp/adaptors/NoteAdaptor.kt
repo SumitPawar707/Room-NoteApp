1b407073e73b69320eff47deee640a9ee5d5ab0d
Sumit Pawar <sp260089@gmail.com>
2025-04-16 13:48:56 +0530

    Created RecyclerView and attached to MainActivity (did not added data just template)

---
+++ app/src/main/java/com/example/room_noteapp/adaptors/NoteAdaptor.kt
@@ -0,0 +1,37 @@
+package com.example.room_noteapp.adaptors
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.room_noteapp.R
+
+class NoteAdaptor(context: Context, var notesList: MutableList<com.example.room_noteapp.Note>):
+    RecyclerView.Adapter<NoteAdaptor.NoteViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_view_item,parent,false)
+        return NoteViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notesList[position]
+        holder.textViewtitle.text=note.title
+        holder.textViewdescription.text=note.description
+        holder.textViewpriority.text=note.priority.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return notesList.size
+    }
+
+
+    inner class NoteViewHolder(view:View):RecyclerView.ViewHolder(view){
+        var textViewtitle=view.findViewById(R.id.text_view_title) as TextView
+        var textViewdescription: TextView = view.findViewById(R.id.text_view_description)
+        var textViewpriority:TextView=view.findViewById(R.id.text_view_priority)
+    }
+}
